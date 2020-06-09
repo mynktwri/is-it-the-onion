074532211fda4991e98a52dd51e08f363c1f81d4
aagusuab <ykim121@ucsc.edu>
2020-06-08 22:03:15 -0700

    edited NLP to use digits instead of string values

--- NLP/analyze.py
+++ NLP/analyze.py
@@ -1,24 +1,50 @@
 import spacy
-from spacy import displacy
 import pandas as pd
 
 spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_sm")
 
 
-# "text", "lemma", "POS", "explain", "stopword" are parsed for now
+# "text", "POS", "stopword" are parsed for now
 def parse(text):
     doc = nlp(text)
     parse_list = []
     for t in doc:
-        row = [t.text, t.pos_, t.is_stop]
+        row = [t.text, convert_pos(t.pos_), convert_stop(t.is_stop)]
         parse_list.append(row)
     pd.DataFrame(parse_list)
     return parse_list
 
-# Load English tokenizer, tagger, parser, NER and word vectors
-# doc = nlp("Brad Pitt took an SAT test and I think i love tests")
-# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
-# for entity in doc.ents:
-#     print(entity.text, entity.label_)
+
+def convert_pos(pos):
+    pos_list = {
+        "ADJ": 1,
+        "ADP": 2,
+        "ADV": 3,
+        "AUX": 4,
+        "CONJ": 5,
+        "CCONJ": 6,
+        "DET": 7,
+        "INTJ": 8,
+        "NOUN": 9,
+        "NUM": 10,
+        "PART": 11,
+        "PRON": 12,
+        "PROPN": 13,
+        "PUNCT": 14,
+        "SCONJ": 15,
+        "SYM": 16,
+        "VERB": 17,
+        "SPACE": 18
+    }
+    if pos not in pos_list:
+        return 19
+    else:
+        return pos_list[pos]
+
+
+def convert_stop(bool):
+    if bool:
+        return 1
+    else:
+        return 0
