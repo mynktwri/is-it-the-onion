90e37c70b53cee5834ceb731e08858a0c8f368e6
Mayank Tiwari <mayank@rockinfamily.com>
2020-06-13 00:43:32 -0700

    working on clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -14,9 +14,15 @@ print(post_title_not_onion.head())
 
 post_title_not_onion.to_csv("notonion_clean.csv")
 
-data = pd.read_csv("data/posts_filtered.csv")
+data = pd.read_csv("data/posts.csv")
 print(data.head())
-post_title_onion = pd.DataFrame(data[['title']])
+data_too = []
+for index, row in data.iterrows():
+    if i['category'] not in ['American Voices', 'Infographic', 'Slideshow', 'Entertainment',
+                    'Editorial Cartoon'] and i['category'] is not None:
+        data_too = data_too.append(i)
+post_title_onion = pd.DataFrame(data_too[['title']])
 post_title_onion["onion"] = 1
 print(post_title_onion.head())
+post_title_onion.to_csv("data/posts_clean.csv")
 #ignore no category,  american voices, infographic, slideshow,
