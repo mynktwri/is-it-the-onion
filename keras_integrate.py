5df7185a7277a6eb1cbfb0699d89bfb07865086b
Mayank Tiwari <mayank@rockinfamily.com>
2020-06-12 19:23:27 -0700

    started word embeddings....

---
+++ keras_integrate.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath) as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word]
+                embedding_matrix[idx] = np.array(
+                    vector, dtype=np.float32)[:embedding_dim]
+
+    return embedding_matrix
+
+#set embedding_dim to 50 to start
+#nonzero_elements = np.count_nonzero(np.count_nonzero(embedding_matrix, axis=1))
+#nonzero_elements / vocab_size
+
+#see how good percent coverage is of the embeddingm atricxxx
