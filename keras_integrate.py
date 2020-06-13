2817022ba2a6dd058cbb7f8bde379fe18d888137
Mayank Tiwari <mayank@rockinfamily.com>
2020-06-12 20:14:29 -0700

    some more changes....

--- keras_integrate.py
+++ keras_integrate.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def create_embedding_matrix(filepath, word_index, embedding_dim):
     vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
@@ -13,6 +14,13 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
                     vector, dtype=np.float32)[:embedding_dim]
 
     return embedding_matrix
+
+
+data = pd.read_csv("data/notonion_clean.csv", index_col=0)
+print(data.head())
+
+
+
 
 #set embedding_dim to 50 to start
 #nonzero_elements = np.count_nonzero(np.count_nonzero(embedding_matrix, axis=1))
